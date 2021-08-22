9e5c689eb6dd5ecdbec74de9d83ea7b9ff2c1d0d
ParkBobby <89330666+ParkBobby@users.noreply.github.com>
2021-08-22 17:07:07 +0900

    Update bithumb_inquiry.py

--- bithumb_inquiry.py
+++ bithumb_inquiry.py
@@ -1,10 +1,21 @@
-import pybithumb
 import time
-con_key = "Your_Key"
+import pybithumb
+
+
+# Type in your key
 sec_key = "Your_Key"
+con_key = "Your_Key"
+
+
+# Bithumb API
 bithumb = pybithumb.Bithumb(con_key,sec_key)
-print(bithumb.get_tickers())
-print("\n")
+
+
+# Possessed coins
+print(bithumb.get_tickers(),"\n")
+
+
+# Current price of possessed coins
 for ticker in bithumb.get_tickers():
     balance = bithumb.get_balance(ticker)
     if balance[0] != 0:
